bf73736ff867757e54da957141f2bbd6c7019057
HelioI <111895831+HelioI@users.noreply.github.com>
2022-10-13 21:31:00 -0300

    Atualizei exercícios

---
+++ ex011/JavaScript.js
@@ -0,0 +1,9 @@
+var idade = 67
+console.log(`Você tem ${idade} anos de idade!`)
+if (idade >= 18 && idade < 65) {
+    console.log('Voto obrigatório.')}
+else if (idade < 16) {
+    console.log('Não vota.')
+} else {
+    console.log('Voto opcional.')
+}
