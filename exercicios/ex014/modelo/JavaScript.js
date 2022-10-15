ba9790056ed58472a68d9ddc2e53e3a24d2711af
HelioI <111895831+HelioI@users.noreply.github.com>
2022-10-15 19:32:10 -0300

    Atualizei exercícios

---
+++ exercicios/ex014/modelo/JavaScript.js
@@ -0,0 +1,7 @@
+function carregar() {
+    let msg = window.document.getElementById('msg')
+    let img = window.document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `Agora são ${}`
+}
---
+++ exercicios/ex016/script.js
@@ -0,0 +1,11 @@
+let c = 1
+let cont = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+do {
+    console.log(`${cont}° passo`)
+    cont++
+} while (cont <= 10)
